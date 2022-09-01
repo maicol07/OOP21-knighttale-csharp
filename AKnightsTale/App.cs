4f8d1c122f88100df476eeda91d435dee2cd5d01
Maicol Battistini <maicolbattistini@live.it>
2022-09-01 22:55:19 +0200

    feat: ✨ Added `App` and `AppPaths`

---
+++ AKnightsTale/App.cs
@@ -0,0 +1,18 @@
+﻿namespace AKnightsTale
+{
+    /// <summary>
+    /// App class. In this implementation 
+    /// </summary>
+    public class App
+    {
+        /// <summary>
+        /// Name of the application.
+        /// </summary>
+        public const string AppName = "A Knight's Tale";
+
+        /// <summary>
+        /// Version of the application.
+        /// </summary>
+        public const string AppVersion = "0.1.0";
+    }
+}
---
+++ AKnightsTale/MaicolBattistini/utils/AppPaths.cs
@@ -0,0 +1,21 @@
+﻿namespace AKnightsTale.MaicolBattistini.utils
+{
+    /// <summary>
+    /// Utility to get application paths.
+    /// </summary>
+    public static class AppPaths
+    {
+        /// <summary>
+        /// Get the path to a file in the application's data folder.
+        /// </summary>
+        /// <param name="pathsToAppend"></param>
+        /// <returns></returns>
+        public static string GetFilePath(params string[] pathsToAppend)
+        {
+            var appDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+                , "unibo", App.AppName, App.AppVersion);
+
+            return Path.Combine(pathsToAppend.Prepend(appDir).ToArray());
+        }
+    }
+}
