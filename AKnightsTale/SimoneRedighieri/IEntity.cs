6fa27bf93f088b4831c5a41fef290bf176062ce0
Simone Redighieri <simone.redighieri@studio.unibo.it>
2022-09-01 23:30:56 +0200

    Created interface Entity

--- AKnightsTale/SimoneRedighieri/IEntity.cs
+++ AKnightsTale/SimoneRedighieri/IEntity.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AKnightsTale.SimoneRedighieri
+﻿namespace AKnightsTale.SimoneRedighieri
 {
     public interface IEntity
     {
+        /// <summary>
+        ///   Gets the entity position
+        /// </summary>
+        /// <returns> the entity position </returns>
+        Point<double> GetPosition();
+
+        /// <summary>
+        ///     Sets the entity position.
+        /// </summary>
+        /// <param name="p"> The new position </param>
+        void SetPosition(Point<double> p);
+
+        /// <summary>
+        ///     Gets the entity borders
+        /// </summary>
+        /// <returns> The entity bounds </returns>
+        Borders GetBorders();
+
+        /// <summary>
+        ///     Gets the entity type
+        /// </summary>
+        /// <returns> The entity type </returns>
+        EntityType GetType();
+
+        /// <summary>
+        ///     Checks if the entity can have collisions
+        /// </summary>
+        /// <returns> true if entity can have collisions, false otherwise </returns>
+        bool IsCollidable();
     }
 }
