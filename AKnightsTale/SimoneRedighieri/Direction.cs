beeb15d196ea53fb6cb18cc82342c13ea3b2e939
Simone Redighieri <simone.redighieri@studio.unibo.it>
2022-09-01 23:24:22 +0200

    Created enum classes

---
+++ AKnightsTale/SimoneRedighieri/Direction.cs
@@ -0,0 +1,13 @@
+﻿namespace AKnightsTale.SimoneRedighieri
+{
+    /// <summary>
+    ///     Directions where a entity can move
+    /// </summary>
+    public enum Direction
+    {
+        Right,
+        Left,
+        Up,
+        Down
+    }
+}
