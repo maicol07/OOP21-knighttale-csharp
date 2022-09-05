c644b77939b07ab8ea189fe9805849b1288dac38
Simone Redighieri <simone.redighieri@studio.unibo.it>
2022-09-05 11:13:11 +0200

    Created class of the player

---
+++ AKnightsTale/SimoneRedighieri/Player.cs
@@ -0,0 +1,23 @@
+﻿namespace AKnightsTale.SimoneRedighieri
+{
+    /// <summary>
+    ///     Class of the Player
+    /// </summary>
+    public class Player : BaseCharacter
+    {
+        private const double PlayerWidth = 50;
+        private const double PlayerHeight = 50.0;
+        private const double PlayerDamage = 25.0;
+        private const double PlayerMaxHealth = 100.0;
+        private const double PlayerSpeed= 1.0;
+        private const double AttackRange = 5.0;
+
+        public Player(Point<double> p) : base(new Borders(p.X, p.Y, PlayerWidth, PlayerHeight), EntityType.Player,
+            true, PlayerSpeed, Direction.Right, PlayerMaxHealth, PlayerDamage)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override double GetAttackRange() => AttackRange;
+    }
+}
