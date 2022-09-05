26247abca54173730a3469846c06271ac05e501c
Simone Redighieri <simone.redighieri@studio.unibo.it>
2022-09-05 11:11:47 +0200

    Created interfaces for attack

---
+++ AKnightsTale/SimoneRedighieri/IAttackEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace AKnightsTale.SimoneRedighieri
+{
+    public interface IAttackEntity : IEntity
+    {
+        /// <summary>
+        ///     Gets and sets the damage of entity
+        /// </summary>
+        double Damage { get; set; }
+
+        /// <summary>
+        ///     Gets the attack range of entity
+        /// </summary>
+        /// <returns> The attack range </returns>
+        double GetAttackRange();
+
+        /// <summary>
+        ///     Attacks another entity and deals damage to him.
+        /// </summary>
+        /// <param name="e"> The other entity </param>
+        void Attack(ILifeEntity e);
+    }
+}
