20312d1daa8703284cab19aba04f3dfa7513af2f
Simone Redighieri <simone.redighieri@studio.unibo.it>
2022-09-05 11:12:03 +0200

    Created interfaces for life

---
+++ AKnightsTale/SimoneRedighieri/ILifeEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace AKnightsTale.SimoneRedighieri
+{
+    public interface ILifeEntity : IEntity, IDefenseEntity
+    {
+
+        /// <summary>
+        ///     Gets and sets the health of entity
+        /// </summary>
+        double Health { get; set; }
+
+        /// <summary>
+        ///     Gets the entity maximum health
+        /// </summary>
+        double MaxHealth { get; }
+
+        /// <summary>
+        ///     Checks if the entity is died or not
+        /// </summary>
+        /// <returns> true if it's died, false if it's alive </returns>
+        bool IsDead();
+    }
+}
