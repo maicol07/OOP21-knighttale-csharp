e2f7436bdd4a47818e5fd85847f8563804a91ac2
Simone Redighieri <simone.redighieri@studio.unibo.it>
2022-09-05 11:11:38 +0200

    Created interfaces for movement

---
+++ AKnightsTale/SimoneRedighieri/IMovableEntity.cs
@@ -0,0 +1,35 @@
+﻿namespace AKnightsTale.SimoneRedighieri
+{
+    public interface IMovableEntity : IEntity 
+    {
+        /// <summary>
+        ///     Gets and sets the entity speed
+        /// </summary>
+        double Speed { get; set; }
+
+        /// <summary>
+        ///     Gets and sets the entity direction
+        /// </summary>
+        Direction Direction { get; set; }
+
+        /// <summary>
+        ///     Moves up the entity
+        /// </summary>
+        void GoUp();
+
+        /// <summary>
+        ///     Moves down the entity
+        /// </summary>
+        void GoDown();
+
+        /// <summary>
+        ///     Moves left the entity
+        /// </summary>
+        void GoLeft();
+
+        /// <summary>
+        ///     Moves right the entity
+        /// </summary>
+        void GoRight();
+    }
+}
