e26ccfee3aae3e97739dad4e66090b2f3a9b8459
Simone Redighieri <simone.redighieri@studio.unibo.it>
2022-09-21 15:10:59 +0200

    Modified constructor

--- AKnightsTale/SimoneRedighieri/model/Player.cs
+++ AKnightsTale/SimoneRedighieri/model/Player.cs
@@ -7,15 +7,16 @@ namespace AKnightsTale.SimoneRedighieri.model
     /// </summary>
     public class Player : BaseCharacter
     {
-        private const double PlayerWidth = 50;
-        private const double PlayerHeight = 50.0;
+        private const double PlayerWidth = 24.0;
+        private const double PlayerHeight = 32.0;
         private const double PlayerDamage = 25.0;
         private const double PlayerMaxHealth = 100.0;
         private const double PlayerSpeed= 1.0;
+        private const double PlayerDefense = 25.0;
         private const double AttackRange = 5.0;
 
-        public Player(Point<double> p) : base(new Borders(p.X, p.Y, PlayerWidth, PlayerHeight), EntityType.Player,
-            true, PlayerSpeed, Direction.Right, PlayerMaxHealth, PlayerDamage)
+        public Player(Point<double> p) : base(new (p.X, p.Y, PlayerWidth, PlayerHeight), EntityType.Player,
+            true, PlayerSpeed, Direction.Right, PlayerMaxHealth, PlayerDamage, PlayerDefense)
         {
         }
 
