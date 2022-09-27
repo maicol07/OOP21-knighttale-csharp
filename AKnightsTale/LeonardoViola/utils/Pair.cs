7ce97dc12674620596796156f6d09e2c37480152
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2022-09-27 22:20:38 +0200

    add class Pair

---
+++ AKnightsTale/LeonardoViola/utils/Pair.cs
@@ -0,0 +1,13 @@
+﻿namespace AKnightsTale.Leonardo_Viola.utils;
+
+public class Pair <TK, TV>
+{
+    public Pair(TK k, TV v)
+    {
+        K = k;
+        V = v;
+    }
+
+    private TK K { get; set; }
+    private TV V { get; set; }
+}
