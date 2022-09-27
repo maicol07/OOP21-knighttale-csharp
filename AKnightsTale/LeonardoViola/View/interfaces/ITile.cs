83d3ee4bacb22f9bf740b6c3b90446ee69bbcbcc
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2022-09-27 22:27:54 +0200

    terminate ITile

---
+++ AKnightsTale/LeonardoViola/View/interfaces/ITile.cs
@@ -0,0 +1,57 @@
+﻿using AKnightsTale.SimoneRedighieri.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKnightsTale.Leonardo_Viola.View.interfaces
+{
+    internal interface ITile : IAnimatedEntityView
+    {
+        
+        /// <summary>
+        /// The tile's index
+        /// </summary>
+        int Index { get; }
+        
+        /// <summary>
+        /// The Width
+        /// </summary>
+        double Width { get; set; }
+        
+        /// <summary>
+        /// The Height
+        /// </summary>
+        double Height { get; set; }
+        
+        /*/// <summary>
+        /// Get the index of the tile.
+        /// Each tile is assigned a unique index to identify the different tiles.
+        /// </summary>
+        /// <returns>the tile's index.</returns>
+        int GetIndex();*/
+     
+        /// <summary>
+        /// get the type of the tile.
+        /// </summary>
+        /// <returns>the tile's type</returns>
+        EntityType GetEntityType();
+
+        /// <summary>
+        /// The type of tile.
+        /// </summary>
+        bool Collidable { get; set; }
+        
+        /// <summary>
+        /// Resize the tile.
+        /// </summary>
+        void Resize();
+
+        /// <summary>
+        /// Reposition the tile.
+        /// </summary>
+        void Reposition();
+    }
+
+}
