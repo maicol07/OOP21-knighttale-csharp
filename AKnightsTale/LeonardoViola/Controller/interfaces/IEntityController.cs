422a9f40696fd8c60310f472d7f249594c9ba613
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2022-09-27 22:23:18 +0200

    add IEntityController.cs

---
+++ AKnightsTale/LeonardoViola/Controller/interfaces/IEntityController.cs
@@ -0,0 +1,29 @@
+﻿using AKnightsTale.Leonardo_Viola.View.interfaces;
+using AKnightsTale.SimoneRedighieri.model;
+
+namespace AKnightsTale.Leonardo_Viola.Controller.interfaces;
+
+public interface IEntityController <TM, TV>
+    where TM : IEntity
+    where TV : IEntityView
+    
+{
+    /// <summary>
+    /// Gets the model of this entity.
+    /// </summary>
+    /// <returns>the model</returns>
+    TM GetModel();
+    
+    /// <summary>
+    /// Gets the view of this entity.
+    /// </summary>
+    /// <returns>the view</returns>
+    TV GetView();
+    
+    /// <summary>
+    /// Adapt position to screen size.
+    /// <param name="traslX">the trasl x</param>
+    /// <param name="traslY">the trasl y</param>
+    /// </summary>
+    void AdaptPositionToScreenSize(double traslX, double traslY);
+}
