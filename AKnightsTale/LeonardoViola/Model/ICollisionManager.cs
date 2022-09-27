96b10ea0d0b4b8642ec1347fd2ab240aaf66b912
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2022-09-27 22:23:44 +0200

    add CollisionManager

---
+++ AKnightsTale/LeonardoViola/Model/ICollisionManager.cs
@@ -0,0 +1,42 @@
+﻿using AKnightsTale.Leonardo_Viola.Controller.interfaces;
+using AKnightsTale.Leonardo_Viola.View.interfaces;
+using AKnightsTale.SimoneRedighieri.model;
+
+namespace AKnightsTale.Leonardo_Viola.Model;
+
+public interface ICollisionManager
+{
+    ///<summary>
+    /// Checks if a character has collisions with other characters.
+    /// <param name="ec">the character to control.</param>
+    /// </summary>
+    /// <returns>a list of characters with whom he has collisions.</returns>
+    List<IEntityController<ICharacter, IAnimatedEntityView>> CheckCollision(
+        IEntityController<ICharacter, IAnimatedEntityView> ec);
+
+    
+    ///<summary>
+    /// Controls in which directions the character can move.
+    /// <param name="ec">the character to control.</param>
+    /// </summary>
+    /// <returns>a list of directions in which it can move</returns>
+    List<Direction> CheckDirections(IEntityController<ICharacter, IAnimatedEntityView> ec);
+        
+    ///<summary>
+    /// Controls the collisions of each character.
+    /// </summary>
+    /// <returns>a list of lists of characters colliding with each other.</returns>
+    List<List<IEntityController<ICharacter, IAnimatedEntityView>>> Update();
+    
+    
+    ///<summary>
+    /// Set the width of screen.
+    /// </summary>
+    double WidthScreen { set; }
+    
+    ///<summary>
+    /// Set the height of screen.
+    /// </summary>
+    double HeightScreen { set; }
+
+}
