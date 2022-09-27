11ee9714cb0231f38d064b460e07b330e7a800b3
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2022-09-27 22:21:44 +0200

    add interface ISpawner

---
+++ AKnightsTale/LeonardoViola/Model/ISpawner.cs
@@ -0,0 +1,11 @@
+﻿using AKnightsTale.Leonardo_Viola.utils;
+
+namespace AKnightsTale.Leonardo_Viola.Model;
+
+public interface ISpawner
+{
+    ///<summary>
+    /// the updated map
+    /// </summary>
+    IDictionary<Pair<int, int>, int> Map { get; }
+}
