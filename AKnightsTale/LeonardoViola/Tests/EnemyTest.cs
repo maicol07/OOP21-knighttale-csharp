e92d70cef055aa900c1be5c82d5c533944a2f011
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2022-09-27 22:22:26 +0200

    add EnemyTest

---
+++ AKnightsTale/LeonardoViola/Tests/EnemyTest.cs
@@ -0,0 +1,65 @@
+﻿using AKnightsTale.Leonardo_Viola.Model;
+using AKnightsTale.SimoneRedighieri.model;
+using AKnightsTale.SimoneRedighieri.utils;
+using FluentAssertions;
+using NUnit.Framework;
+namespace AKnightsTale.Leonardo_Viola.Tests;
+
+/// <summary>
+/// Tests for the Enemy class.
+/// </summary>
+[TestFixture]
+public class EnemyTest
+{
+    private static readonly int SampleCoordinate = 30;
+
+    /// <summary>
+    /// Checks the enemy's type.
+    /// </summary>
+    [Test]
+    public void CheckType() {
+        var enemy = GetEnemy();
+        enemy.GetPosition().Should().Be(EntityType.Enemy);
+    }
+
+    /// <summary>
+    /// Move player Axis X".
+    /// </summary>
+    [Test]
+    public void CheckMovementAxisX() {
+        var enemy = GetEnemy();
+        var position = enemy.GetPosition();
+        enemy.GoLeft();
+        position = new Point<double>(position.X - enemy.Speed, position.Y);
+        position.Should().Be(enemy.GetPosition());
+    }
+
+    /// <summary>
+    /// Move player Axis Y".
+    /// </summary>
+    [Test]
+    public void CheckMovementAxisY() {
+        var enemy = GetEnemy();
+        var position = enemy.GetPosition();
+        enemy.GoUp();
+        position = new Point<double>(position.X, position.Y - enemy.Speed);
+        position.Should().Be(enemy.GetPosition());
+    }
+
+    /// <summary>
+    /// Check position.
+    /// </summary>
+    [Test]
+    public void CheckPosition()
+    {
+        var enemy = GetEnemy();
+        enemy.GetPosition().Should().Be(new Point<double>(10, 10));
+    }
+
+    /// <summary>
+    /// Returns the enemy to use for testing. 
+    /// </summary>
+    /// <returns> The player. </returns>
+    private static ICharacter GetEnemy() => new Enemy(new Point<double>(SampleCoordinate, SampleCoordinate));
+
+}
